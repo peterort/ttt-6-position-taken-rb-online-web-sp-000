8034cf17d5ff6d201bf9c8a3ebcbef89c3e96d99
Peter Ort <peter@peterort.com>
2020-02-25 18:05:11 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+ if board[index] == " " || board[index] == ""
+   return false
+
+ elsif board[index] == nil
+   return false
+
+ elsif board[index] == "X" || "O"
+   return true
+
+else return nil
+
+ end
+
+end
